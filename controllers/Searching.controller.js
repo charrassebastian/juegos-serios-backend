e2e4e312b27cdd59eb95cc1712080b3c67442eb1
Sebastian Horacio Charras <sebastianhoraciocharras@gmail.com>
2023-10-09 20:13:13 +0000

    Now the searching controller allows the user to filter by area, purpose, market, public and content validation

--- controllers/Searching.controller.js
+++ controllers/Searching.controller.js
@@ -4,7 +4,6 @@ const Game = require('../models/Game.schema')
  * Endpoint for retrieving the saved games
  */
 module.exports.getGames = async (req, res) => {
-    // TODO: if the user wants to show only validated content, filter the games accordingly
     try {
         const games = await Game.find()
         if (games) {
@@ -17,20 +16,35 @@ module.exports.getGames = async (req, res) => {
                 filteredGames = []
                 const value = req.query.value ?? ""
                 const regexArg = req.query.value ? ".*" + value + ".*" : ".*"
-                const regex = new RegExp(regexArg, "i") // this is not very flexible
+                const regex = new RegExp(regexArg, "i")
                 console.log('regex')
                 console.log(regex)
                 games.forEach(game => {
                     if (game.name.match(regex)) {
                         filteredGames.push({ game, priority: 1 })
-                    } else if (game.area.some(area => area.match(regex))) {
-                        filteredGames.push({ game, priority: 2 })
                     } else if (game.description.match(regex)) {
+                        filteredGames.push({ game, priority: 2 })
+                    } else if (game.observationsAndSuggestions.match(regex)) {
                         filteredGames.push({ game, priority: 3 })
                     } else if (game.others.match(regex)) {
                         filteredGames.push({ game, priority: 4 })
                     }
                 })
+                if(req.query.onlyValidatedContent){
+                    filteredGames = filteredGames.filter(game => game.contentValidation)
+                }
+                if(req.query.area){
+                    filteredGames = filteredGames.filter(game => game.area.includes(req.query.area))
+                }
+                if(req.query.purpose){
+                    filteredGames = filteredGames.filter(game => game.purpose.includes(req.query.purpose))
+                }
+                if(req.query.market){
+                    filteredGames = filteredGames.filter(game => game.scope.market.includes(req.query.market))
+                }
+                if(req.query.public){
+                    filteredGames = filteredGames.filter(game => game.scope.public.includes(req.query.public))
+                }
                 console.log('filtered games')
                 console.log(filteredGames)
                 const orderedGames = []
