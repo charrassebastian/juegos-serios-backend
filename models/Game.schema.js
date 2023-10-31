d5d458c9950e054641b49588284ebe0864636b9d
Sebastian Horacio Charras <sebastiancharras@outlook.com>
2023-10-31 20:41:21 +0000

    removed the goal field for the game and added the playabilityEvaluation and playabilityJustification fields

--- models/Game.schema.js
+++ models/Game.schema.js
@@ -12,13 +12,15 @@ const Game = new Schema({
     "purpose": [String],
     "scope": Scope,
     "hasGoal": Boolean,
+    "goal": String,
     "description": { type: String, require: true },
     "link": { type: String },
     "contentValidation": { type: String },
     "observationsAndSuggestions": { type: String },
-    "score": { type: Number },
     "imageLink": { type: String },
-    "others": { type: String }
+    "others": { type: String },
+    "playabilityEvaluation": { type: String },
+    "playabilityJustification": { type: String },
 })
 
 module.exports = mongoose.model('Game', Game)
